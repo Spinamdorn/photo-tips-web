80c28ec2927ae1faf8e8d4e3f3af0a30b69af8c6
Spinamdorn <spinamdorn@gmail.com>
2021-01-14 19:07:29 +0500

    added update info and change password

--- src/components/Account/Account.js
+++ src/components/Account/Account.js
@@ -1,78 +1,192 @@
-import React, { Component, useState } from "react";
+import React, { useState, useEffect } from "react";
 import "../../css/account.css";
 
-export default class Account extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      token: props.token,
-      name: "",
-      surname: "",
-      email: "",
-      phoneNumber: "",
-      registrationDate: "",
-      residenceCity: "",
-    };
-  }
+export default function Account({ token, setToken }) {
+  const [currentToken, setCurrentToken] = useState(token);
+  const [isLoading, setIsLoading] = useState(false);
+  const [isError, setIsError] = useState(false);
+  const [name, setName] = useState();
+  const [surname, setSurname] = useState();
+  const [email, setEmail] = useState();
+  const [phoneNumber, setPhoneNumber] = useState();
+  const [newInfo, setNewInfo] = useState({
+    token: currentToken,
+    name: name,
+    surname: surname,
+    email: email,
+    phoneNumber: phoneNumber,
+  });
 
-  componentDidMount() {
-    fetch("http://85.208.208.156:5000/api/user?Token=" + this.props.token)
-      .then((response) => response.json())
-      .then((data) => {
-        this.setState({
-          name: data["name"],
-          surname: data["surname"] !== "string" ? data["surname"] : "",
-          email: data["email"] !== "string" ? data["email"] : "",
-          phoneNumber: data["email"] !== "string" ? data["phoneNumber"] : "",
-          registrationDate: data["registrationDate"],
-          residenceCity: data["residenceCity"] ? data["residenceCity"] : "",
+  useEffect(() => {
+    async function fetchData() {
+      setIsError(false);
+      setIsLoading(true);
+      try {
+        const response = await fetch("http://85.208.208.156:5000/api/user?Token=" + currentToken);
+        const json = await response.json();
+        setName(json.name);
+        setSurname(json.surname);
+        setEmail(json.email);
+        setPhoneNumber(json.phoneNumber);
+        setIsLoading(false);
+      } catch (error) {
+        setIsError(true);
+      }
+    }
+    fetchData();
+  }, [currentToken]);
+
+  useEffect(() => {
+    setNewInfo({
+      token: currentToken,
+      name: name,
+      surname: surname,
+      email: email,
+      phoneNumber: phoneNumber,
+    });
+  }, [currentToken, name, surname, email, phoneNumber]);
+
+  const submitUpdateInfo = (e) => {
+    e.preventDefault();
+    async function fetchUpdateInfo() {
+      setIsError(false);
+      setIsLoading(true);
+      try {
+        const response = await fetch("http://85.208.208.156:5000/api/user/updateInfo", {
+          method: "PUT",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(newInfo),
         });
-      });
-  }
+        const json = await response.json();
+        var newToken = json.token;
+        if (newToken !== "User not found") {
+          setCurrentToken(newToken);
+          setToken(newToken);
+        }
+        setIsLoading(false);
+      } catch (error) {
+        setIsError(true);
+      }
+    }
+    fetchUpdateInfo();
+  };
 
-  render() {
-    return (
-      <div className='account container'>
-        <div className='frame main-info'>
-          <img src='../../images/user_photo.png' alt='User' />
-          <div>
-            <h2 className='UI'>{this.state.name + " " + this.state.surname}</h2>
-            <p className='UI email'>{this.state.email}</p>
+  const [oldPassword, setOldPassword] = useState(null);
+  const [newPassword, setNewPassword] = useState(null);
+  const [changePasswordBody, setChangePasswordBody] = useState({
+    token: currentToken,
+    oldPassword: oldPassword,
+    newPassword: newPassword,
+  });
+  useEffect(() => {
+    setChangePasswordBody({
+      token: currentToken,
+      oldPassword: oldPassword,
+      newPassword: newPassword,
+    });
+  }, [currentToken, oldPassword, newPassword]);
+  const submitChangePasword = (e) => {
+    async function fetchChangePassword() {
+      setIsError(false);
+      setIsLoading(true);
+      try {
+        const response = await fetch("http://85.208.208.156:5000/api/user/updatePassword", {
+          method: "PUT",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(changePasswordBody),
+        });
+        const json = await response.json();
+        var newToken = json.token;
+        if (newToken !== "User not found") {
+          setCurrentToken(newToken);
+          setToken(newToken);
+        }
+        setIsLoading(false);
+      } catch (error) {
+        setIsError(true);
+      }
+    }
+    e.preventDefault();
+    fetchChangePassword();
+  };
+  return (
+    <div className='account container'>
+      {isLoading ? (
+        <div>Loading ...</div>
+      ) : (
+        <div>
+          <div className='frame main-info'>
+            <img src='../../images/user_photo.png' alt='User' />
+            <div>
+              <h2 className='UI'>{name + " " + surname}</h2>
+              <p className='UI email'>{email}</p>
+            </div>
+          </div>
+          <div className='frame full-info'>
+            <h2 className='UI'>Персональная информация</h2>
+            <form onSubmit={submitUpdateInfo}>
+              <label htmlFor='userName'>
+                <p className='UI caption'>Имя</p>
+              </label>
+              <input
+                id='userName'
+                type='text'
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+              />
+              <label htmlFor='userSurname'>
+                <p className='UI caption'>Фамилия</p>
+              </label>
+              <input
+                id='userSurname'
+                type='text'
+                value={surname}
+                onChange={(e) => setSurname(e.target.value)}
+              />
+              <label htmlFor='userPhone'>
+                <p className='UI caption'>Номер телефона</p>
+              </label>
+              <input
+                id='userPhone'
+                type='text'
+                value={phoneNumber}
+                onChange={(e) => setPhoneNumber(e.target.value)}
+              />
+              {isError && <div>Something went wrong ...</div>}
+              <input type='submit' value='Сохранить' />
+            </form>
+          </div>
+          <div className='frame change-password'>
+            <h2 className='UI'>Изменение пароля</h2>
+            <form onSubmit={submitChangePasword}>
+              <label htmlFor='userPassword'>
+                <p className='UI caption'>Текущий пароль</p>
+              </label>
+              <input
+                id='userPassword'
+                type='password'
+                autoComplete='current-password'
+                onChange={(e) => setOldPassword(e.target.value)}
+              />
+              <label htmlFor='newPassword'>
+                <p className='UI caption'>Новый пароль</p>
+              </label>
+              <input
+                id='newPassword'
+                type='password'
+                autoComplete='new-password'
+                onChange={(e) => setNewPassword(e.target.value)}
+              />
+              {isError && <div>Something went wrong ...</div>}
+              <input type='submit' value='Изменить пароль' />
+            </form>
           </div>
         </div>
-        <div className='frame full-info'>
-          <h2 className='UI'>Персональная информация</h2>
-          <form>
-            <label for='userName'>
-              <p className='UI caption'>Имя</p>
-            </label>
-            <input id='userName' type='text' value={this.state.name} />
-            <label for='userSurname'>
-              <p className='UI caption'>Фамилия</p>
-            </label>
-            <input id='userSurname' type='text' value={this.state.surname} />
-            <label for='userPhone'>
-              <p className='UI caption'>Номер телефона</p>
-            </label>
-            <input id='userPhone' type='text' value={this.state.phoneNumber} />
-            <input type='submit' value='Сохранить' />
-          </form>
-        </div>
-        <div className='frame change-password'>
-          <h2 className='UI'>Изменение пароля</h2>
-          <form>
-            <label for='userPassword'>
-              <p className='UI caption'>Текущий пароль</p>
-            </label>
-            <input id='userPassword' type='password' autocomplete='current-password' />
-            <label for='newPassword'>
-              <p className='UI caption'>Новый пароль</p>
-            </label>
-            <input id='newPassword' type='password' autocomplete='new-password' />
-            <input type='submit' value='Изменить пароль' />
-          </form>
-        </div>
-      </div>
-    );
-  }
+      )}
+    </div>
+  );
 }
