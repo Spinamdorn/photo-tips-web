46d4b7f91990070fe84908c12876a96490e9c861
Spinamdorn <spinamdorn@gmail.com>
2021-01-14 16:15:06 +0500

    added video via api

--- src/components/Lecture/LessonSubpartVideo.js
+++ src/components/Lecture/LessonSubpartVideo.js
@@ -1,10 +1,11 @@
 import React from "react";
+import ReactPlayer from "react-player/file";
 
 function LessonSubpartVideo({ content }) {
   var src = "http://85.208.208.156:5000/" + content;
   return (
     <div className='lesson-subpart video-container'>
-      {src}
+      <ReactPlayer url={src} width='100%' height='100%' controls={true} />
       <div className='title'>
         <p className='article'>Лучшее видео в мире</p>
       </div>
